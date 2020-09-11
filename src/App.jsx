58bfc182b11d597b414636690896c0456c6aead3
KozaX <macpedrot@gmail.com>
2020-09-11 15:17:19 +0200

    add header and tabs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <section className="Header">
+        <div>
+          <h1>Pedro Machado</h1>
+        </div>
+        <div>
+          {/* <img src="/images/cover.jpg" alt="cover" /> */}
+          <img src="#" alt="cover" />
+        </div>
+        <div>
+          <h2>Fullstack Developer</h2>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>About me</h3>
+          <h3>Skills</h3>
+          <h3>Contact</h3>
+        </div>
+      </section>
     </div>
   );
 }
